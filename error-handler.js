1283e77b6231b3680c1f85c3951f10e1549c5686
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-28 10:44:37 +0100

    sets up express app - inc. adds listner, error handler and mvc skeleton

---
+++ error-handler.js
@@ -0,0 +1,15 @@
+exports.handleBadRequest = (err, req, res, next) => {
+    if (err.code) {
+        res.status(400).send({ msg: 'Bad request' });
+    } else next(err);
+}
+
+exports.handleErrorMessage = (err, req, res, next) => {
+    if (err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+}
+
+exports.handleServerError = (err, req, res, next) => {
+    res.status(500).send({ msg: 'Server error'})
+}
