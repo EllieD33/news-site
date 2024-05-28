301ad2d3ea5b1f4bde6563f9cca497593b234e18
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-28 12:46:47 +0100

    adds and tests get topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,5 +4,20 @@ const db = require('../db/connection');
 const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data/index')
 
-beforeEach(() => seed({ data }));
+beforeEach(() => seed(data));
 afterAll(() => db.end());
+
+describe('/api/topics', () => {
+    test('GET:200 sends an array of topic objects to the client', () => {
+        return request(app)
+            .get('/api/topics')
+            .expect(200)
+            .then((response)=> {
+                expect(response.body.topics.length).toBe(3);
+                response.body.topics.forEach((topic) => {
+                    expect(topic).toHaveProperty('description', expect.any(String));
+                    expect(topic).toHaveProperty('slug', expect.any(String));
+                })
+            }) 
+    });
+});
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { handleBadRequest, handleErrorMessage, handleServerError } = require('./error-handler')
+const { getAllTopics } = require('./mvc/controllers/topics.controller')
 const app = express();
 
 app.use(express.json());
+
+app.get('/api/topics', getAllTopics);
 
 app.use(handleBadRequest);
 app.use(handleErrorMessage);
