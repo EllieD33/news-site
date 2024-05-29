40bc384178f6f0d5e012a9502632abd2265bf15f
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-29 15:36:50 +0100

    refactors to consistently stick to promise chains

--- mvc/models/articles.models.js
+++ mvc/models/articles.models.js
@@ -1,16 +1,16 @@
 const db = require('../../db/connection');
 const format = require("pg-format");
 
-const checkExists = async (table, column, value) => {
+const checkExists = (table, column, value) => {
     const queryStr = format("SELECT * FROM %I WHERE %I = $1;", table, column);
-    const dbOutput = await db.query(queryStr, [value]);
-
-    if (dbOutput.rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Not found" });
-    }
+    return db.query(queryStr, [value]).then((dbOutput) => {
+        if (dbOutput.rows.length === 0) {
+            return Promise.reject({ status: 404, msg: "Not found" });
+        }
+    });
 }
 
-exports.fetchArticleById = async (id) => {    
+exports.fetchArticleById = (id) => {    
     const selectQuery = `SELECT 
         articles.author, 
         articles.title, 
@@ -26,14 +26,14 @@ exports.fetchArticleById = async (id) => {
     WHERE articles.article_id = $1
     GROUP BY articles.article_id;`
 
-    await checkExists('articles', 'article_id', id)
-
-    return db.query(selectQuery, [id]).then((result) => {
-        return result.rows[0]
-    })
+    return checkExists('articles', 'article_id', id).then(() => {
+        return db.query(selectQuery, [id]);
+    }).then((result) => {
+        return result.rows[0];
+    });
 }
 
-exports.fetchArticles = async (topic) => {   
+exports.fetchArticles = (topic) => {   
     let selectQuery = `SELECT 
         articles.author, 
         articles.title, 
@@ -48,23 +48,28 @@ exports.fetchArticles = async (topic) => {
     const queryValues = [];
 
     if (topic) {
-        const topics = await db.query(`SELECT slug FROM topics;`)
-        validTopics = topics.rows.map((topic) => Object.values(topic)).flat()
-        if (!validTopics.includes(topic)) {
-            return Promise.reject({ status: 400, msg: "Bad request" });
-        } else {
-            selectQuery += ' WHERE topic = $1'
-            queryValues.push(topic)
-        }
-    }
-    
-    selectQuery += ` GROUP BY articles.article_id
-        ORDER BY articles.created_at DESC`
+        return db.query(`SELECT slug FROM topics;`).then((topics) => {
+            const validTopics = topics.rows.map((topic) => Object.values(topic)).flat();
+            if (!validTopics.includes(topic)) {
+                return Promise.reject({ status: 400, msg: "Bad request" });
+            } else {
+                selectQuery += ' WHERE topic = $1';
+                queryValues.push(topic);
+            }
+            selectQuery += ` GROUP BY articles.article_id
+            ORDER BY articles.created_at DESC`;
 
-    return db.query(selectQuery, queryValues)
+            return db.query(selectQuery, queryValues);
+        });
+    } else {
+        selectQuery += ` GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC`;
+
+        return db.query(selectQuery, queryValues);
+    }
 }
 
-exports.fetchComments = async (id) => {
+exports.fetchComments = (id) => {
     if (isNaN(id)) {
         return Promise.reject({
             status: 400,
@@ -72,17 +77,19 @@ exports.fetchComments = async (id) => {
         });
     }
 
-    await checkExists('articles', 'article_id', id); 
-
-    const selectQuery = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`
-    return db.query(selectQuery, [id]).then((result) => {
+    return checkExists('articles', 'article_id', id).then(() => {
+        const selectQuery = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`;
+        return db.query(selectQuery, [id]);
+    }).then((result) => {
         if (result.rows.length === 0) {
-            return []
-        } else return result.rows
-    })
+            return [];
+        } else {
+            return result.rows;
+        }
+    });
 }
 
-exports.insertComment = async (article_id, author, body) => {
+exports.insertComment = (article_id, author, body) => {
     if (!article_id || !author || !body) {
         return Promise.reject({
             status: 400,
@@ -90,15 +97,16 @@ exports.insertComment = async (article_id, author, body) => {
         });
     }
 
-    await checkExists('articles', 'article_id', article_id);
-    await checkExists('users', 'username', author); 
-
-    const formattedComment = [article_id, author, body]
-    const insertQuery = format(`INSERT INTO comments (article_id, author, body) VALUES ($1, $2, $3) RETURNING *;`)
-    return db.query(insertQuery, formattedComment)
+    return checkExists('articles', 'article_id', article_id).then(() => {
+        return checkExists('users', 'username', author);
+    }).then(() => {
+        const formattedComment = [article_id, author, body];
+        const insertQuery = format(`INSERT INTO comments (article_id, author, body) VALUES ($1, $2, $3) RETURNING *;`);
+        return db.query(insertQuery, formattedComment);
+    });
 }
 
-exports.updateVotes = async (article_id, votes) => {
+exports.updateVotes = (article_id, votes) => {
     if (!article_id || !votes) {
         return Promise.reject({
             status: 400,
@@ -106,16 +114,15 @@ exports.updateVotes = async (article_id, votes) => {
         });
     }
 
-    await checkExists('articles', 'article_id', article_id);
-
-    return db.query(`SELECT votes FROM articles WHERE article_id = $1`, [article_id])
-        .then((result) => {
-            updatedVotes = result.rows[0].votes + votes
-            return db.query(`UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *;`, [updatedVotes, article_id])
-        })
+    return checkExists('articles', 'article_id', article_id).then(() => {
+        return db.query(`SELECT votes FROM articles WHERE article_id = $1`, [article_id]);
+    }).then((result) => {
+        const updatedVotes = result.rows[0].votes + votes;
+        return db.query(`UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *;`, [updatedVotes, article_id]);
+    });
 }
 
-exports.removeComment = async (comment_id) => {
+exports.removeComment = (comment_id) => {
     if (!comment_id) {
         return Promise.reject({
             status: 400,
@@ -123,7 +130,7 @@ exports.removeComment = async (comment_id) => {
         });
     }
 
-    await checkExists('comments', 'comment_id', comment_id);
-
-    return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id]);
+    return checkExists('comments', 'comment_id', comment_id).then(() => {
+        return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id]);
+    });
 }
