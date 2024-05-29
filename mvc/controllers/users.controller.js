50fdecea02d7b304987c5981c8ee0702c908e2cb
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-29 15:22:21 +0100

    edits users model and controller logic

--- mvc/controllers/users.controller.js
+++ mvc/controllers/users.controller.js
@@ -2,7 +2,7 @@ const { fetchUsers } = require('../models/users.models')
 
 exports.getUsers = (req, res, next) => {
     fetchUsers().then((result) => {
-        const users = result.rows
+        const users = result
         res.status(200).send({ users })
     }).catch(next)
 }
