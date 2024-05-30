f7596d965c991f2f7f518b542992e910acd10363
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-30 10:33:29 +0100

    adds users router

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const { handleBadRequest, handleErrorMessage, handleServerError } = require('./error-handler')
-const { getUsers } = require('./mvc/controllers/users.controller')
-const app = express();
-
-app.use(express.json());
 
 const apiRouter = require('./routes/api.router');
 const topicsRouter = require('./routes/topics.router');
 const articlesRouter = require('./routes/articles.router');
 const commentsRouter = require('./routes/comments.router');
+const usersRouter = require('./routes/users.router');
+
+const app = express();
+
+app.use(express.json());
 
 app.use('/api', apiRouter);
-app.use('/api/topics', topicsRouter)
-app.use('/api/articles', articlesRouter)
+app.use('/api/topics', topicsRouter);
+app.use('/api/articles', articlesRouter);
 app.use('/api/comments', commentsRouter);
-
-app.get('/api/users', getUsers);
+app.use('/api/users', usersRouter);
 
 app.use(handleBadRequest);
 app.use(handleErrorMessage);
