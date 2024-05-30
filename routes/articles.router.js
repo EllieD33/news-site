8aff2ba20b5eb1fe1d7c0614b6f74586229c88c0
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-30 14:30:25 +0100

    adds post articles

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -1,7 +1,8 @@
 const articlesRouter = require('express').Router()
-const { getArticleById, getArticles, getComments, addComment, updateArticle } = require('../mvc/controllers/articles.controller')
+const { getArticleById, getArticles, getComments, addComment, updateArticle, addArticle } = require('../mvc/controllers/articles.controller')
 
 articlesRouter.get('/', getArticles)
+articlesRouter.post('/', addArticle)
 
 articlesRouter.get('/:article_id', getArticleById);
 articlesRouter.patch('/:article_id', updateArticle);
