a1441e7629427e80e1b0e1fdd9f46dc2325f70da
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-30 10:29:59 +0100

    adds comments router

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { handleBadRequest, handleErrorMessage, handleServerError } = require('./error-handler')
-const { deleteComment } = require('./mvc/controllers/articles.controller')
 const { getUsers } = require('./mvc/controllers/users.controller')
 const app = express();
 
@@ -8,13 +7,13 @@ app.use(express.json());
 
 const apiRouter = require('./routes/api.router');
 const topicsRouter = require('./routes/topics.router');
-const articlesRouter = require('./routes/articles.router')
+const articlesRouter = require('./routes/articles.router');
+const commentsRouter = require('./routes/comments.router');
 
 app.use('/api', apiRouter);
 app.use('/api/topics', topicsRouter)
 app.use('/api/articles', articlesRouter)
-
-app.delete('/api/comments/:comment_id', deleteComment);
+app.use('/api/comments', commentsRouter);
 
 app.get('/api/users', getUsers);
 
