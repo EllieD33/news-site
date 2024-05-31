42b6a35a05d23ab0d3f7541338803c7a6b2a0af9
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-05-31 14:00:31 +0100

    refactors articles controller to improve consistency across variables

--- mvc/controllers/articles.controller.js
+++ mvc/controllers/articles.controller.js
@@ -16,58 +16,58 @@ exports.getArticles = (req, res, next) => {
 }
 
 exports.getComments = (req, res, next) => {
-    const { article_id } = req.params;
+    const { article_id: articleId } = req.params;
     const { limit, page: pageQuery } = req.query;
     const page = parseInt(pageQuery) || 1;
 
-    fetchComments(article_id, limit, page).then((comments) => {
+    fetchComments(articleId, limit, page).then((comments) => {
         res.status(200).send({ comments });
     }).catch(next);
 }
 
 exports.addComment = (req, res, next) => {
-    const { article_id } = req.params;
+    const { article_id: articleId } = req.params;
     const { username, body } = req.body;
 
-    insertComment(article_id, username, body).then((comment) => {
+    insertComment(articleId, username, body).then((comment) => {
         res.status(201).send({ comment });
     }).catch(next);
 }
 
 exports.updateArticle = (req, res, next) => {
-    const { article_id } = req.params;
+    const { article_id: articleId } = req.params;
     const {inc_votes: votes} = req.body;
 
-    updateArticleVotes(article_id, votes).then((article) => {
+    updateArticleVotes(articleId, votes).then((article) => {
         res.status(200).send({ article });
     }).catch(next);
 }
 
 exports.deleteComment = (req, res, next) => {
-    const { comment_id } = req.params;
-    removeComment(comment_id).then(() => {
+    const { comment_id: commentId } = req.params;
+    removeComment(commentId).then(() => {
         res.sendStatus(204)
     }).catch(next);
 }
 
 exports.updateComment = (req, res, next) => {
-    const { comment_id } = req.params;
+    const { comment_id: commentId } = req.params;
     const { inc_votes: votes }= req.body;
-    updateCommentVotes(comment_id, votes).then((comment) => {
+    updateCommentVotes(commentId, votes).then((comment) => {
         res.status(200).send({ comment });
     }).catch(next);
 }
 
 exports.addArticle = (req, res, next) => {
-    const { author, title, body, topic, article_img_url } = req.body
-    postArticle(author, title, body, topic, article_img_url).then((article) => {        
+    const { author, title, body, topic, article_img_url: imageUrl } = req.body
+    postArticle(author, title, body, topic, imageUrl).then((article) => {        
         res.status(201).send({ article })
     }).catch(next)
 }
 
 exports.deleteArticle = (req, res, next) => {
-    const { article_id } = req.params
-    removeArticle(article_id).then(() => {
+    const { article_id: articleId } = req.params
+    removeArticle(articleId).then(() => {
         res.sendStatus(204)
     }).catch(next)
 }
